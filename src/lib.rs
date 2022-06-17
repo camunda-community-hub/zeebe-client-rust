1f06e945f8b526215a39b019cb268bc3d46cadc8
pihme <pihme@users.noreply.github.com>
2022-06-17 17:06:07 +0200

    feat: implement topology request

---
+++ src/lib.rs
@@ -0,0 +1,131 @@
+extern crate core;
+
+pub mod gateway_protocol {
+    tonic::include_proto!("gateway_protocol");
+}
+
+pub mod topology;
+
+use crate::topology::{
+    BrokerInfo, PartitionBrokerHealth, PartitionBrokerRole, PartitionInfo, Topology,
+};
+use futures::executor::block_on;
+use gateway_protocol::gateway_client::GatewayClient;
+use gateway_protocol::TopologyRequest;
+use tonic::transport::Channel;
+
+pub struct ZeebeClient {
+    client: GatewayClient<Channel>,
+}
+
+impl ZeebeClient {
+    pub fn default_client() -> ZeebeClient {
+        let channel = block_on(
+            Channel::from_static("http://[::1]:26500")
+                //.tls_config(tls)?
+                .connect(),
+        );
+
+        let channel = channel.unwrap();
+
+        let client = GatewayClient::new(channel);
+
+        ZeebeClient { client }
+    }
+
+    pub async fn topology(&mut self) -> Result<Topology, tonic::Status> {
+        let response = self.client.topology(TopologyRequest {}).await;
+
+        match response {
+            Ok(response) => Ok(response.into_inner().to_external_representation()),
+            Err(e) => Err(e),
+        }
+    }
+}
+
+pub fn default_client() -> ZeebeClient {
+    let channel = block_on(
+        Channel::from_static("http://[::1]:26500")
+            //.tls_config(tls)?
+            .connect(),
+    );
+
+    let channel = channel.unwrap();
+
+    let client = GatewayClient::new(channel);
+
+    ZeebeClient { client }
+}
+
+impl gateway_protocol::TopologyResponse {
+    fn to_external_representation(&self) -> Topology {
+        let brokers = self
+            .brokers
+            .iter()
+            .map(|broker| broker.to_external_representation())
+            .collect::<Vec<BrokerInfo>>();
+
+        Topology {
+            brokers,
+            cluster_size: self.cluster_size,
+            partition_count: self.partitions_count,
+            replication_factor: self.replication_factor,
+            gateway_version: self.gateway_version.clone(),
+        }
+    }
+}
+
+impl gateway_protocol::BrokerInfo {
+    fn to_external_representation(&self) -> BrokerInfo {
+        let partitions = self
+            .partitions
+            .iter()
+            .map(|partition| partition.to_external_representation())
+            .collect::<Vec<PartitionInfo>>();
+
+        BrokerInfo {
+            node_id: self.node_id,
+            host: self.host.clone(),
+            port: self.port,
+            version: self.version.clone(),
+            partitions,
+        }
+    }
+}
+
+impl gateway_protocol::Partition {
+    fn to_external_representation(&self) -> PartitionInfo {
+        PartitionInfo {
+            partition_id: self.partition_id,
+            role: match gateway_protocol::partition::PartitionBrokerRole::from_i32(self.role) {
+                Some(gateway_protocol::partition::PartitionBrokerRole::Leader) => {
+                    PartitionBrokerRole::LEADER
+                }
+                Some(gateway_protocol::partition::PartitionBrokerRole::Follower) => {
+                    PartitionBrokerRole::FOLLOWER
+                }
+                Some(gateway_protocol::partition::PartitionBrokerRole::Inactive) => {
+                    PartitionBrokerRole::INACTIVE
+                }
+                other => {
+                    panic!("Unknown value {:?}", other);
+                }
+            },
+            health: match gateway_protocol::partition::PartitionBrokerHealth::from_i32(self.health)
+            {
+                Some(gateway_protocol::partition::PartitionBrokerHealth::Healthy) => {
+                    PartitionBrokerHealth::HEALTHY
+                }
+                Some(gateway_protocol::partition::PartitionBrokerHealth::Unhealthy) => {
+                    PartitionBrokerHealth::UNHEALTHY
+                }
+                Some(gateway_protocol::partition::PartitionBrokerHealth::Dead) => {
+                    PartitionBrokerHealth::DEAD
+                }
+                other => {
+                    panic!("Unknown value {:?}", other);
+                }
+            },
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,37 @@
 pub mod gateway_protocol {
     tonic::include_proto!("gateway_protocol");
 }
-
+use futures::executor::block_on;
 use gateway_protocol::gateway_client::GatewayClient;
 use gateway_protocol::TopologyRequest;
 use std::time::Duration;
-use tonic::transport::Channel;
+use tonic::transport::{Channel, Error};
+
+use zeebe_client::topology::{PartitionBrokerHealth, PartitionBrokerRole, PartitionInfo};
+use zeebe_client::ZeebeClient;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let zeebe_client = ZeebeClient::default_client();
 
+    println!("Topology request - pure GRPC");
+    get_topology_pure_grpc().await?;
+
+    println!("Topology request - API");
+    get_topology_api(zeebe_client);
+
+    let part = PartitionInfo {
+        partition_id: 0,
+        role: PartitionBrokerRole::LEADER,
+        health: PartitionBrokerHealth::HEALTHY,
+    };
+
+    println!("{:?}", part);
+
+    Ok(())
+}
+
+async fn get_topology_pure_grpc() -> Result<bool, Error> {
     //let server_root_ca_cert = tokio::fs::read("examples/data/tls/ca.pem").await?;
     //let server_root_ca_cert = Certificate::from_pem(server_root_ca_cert);
     //let client_cert = tokio::fs::read("examples/data/tls/client1.pem").await?;
@@ -20,17 +42,16 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     //    .domain_name("localhost")
     //    .ca_certificate(server_root_ca_cert)
     //    .identity(client_identity);
-        
+
     let channel = Channel::from_static("http://[::1]:26500")
-    //.tls_config(tls)?
-    .timeout(Duration::from_secs(5))
-    .connect()
-    .await?;
+        //.tls_config(tls)?
+        .timeout(Duration::from_secs(5))
+        .connect()
+        .await?;
 
     let mut client = GatewayClient::new(channel);
 
-    let mut request = tonic::Request::new(TopologyRequest {
-    });
+    let mut request = tonic::Request::new(TopologyRequest {});
     request.set_timeout(Duration::from_secs(1));
 
     let response = client.topology(request).await;
@@ -39,10 +60,15 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         Ok(response) => {
             let topology = response.into_inner();
             println!("SUMMARY: {:?}", topology);
-            println!("ClusterSize: {:?}", topology.cluster_size)
         }
         Err(e) => println!("something went wrong: {:?}", e),
     }
 
-    Ok(())
+    Ok(true)
+}
+
+fn get_topology_api(mut zeebe_client: ZeebeClient) {
+    let topology = block_on(zeebe_client.topology());
+
+    println!("SUMMARY: {:?}", topology);
 }
