0eef4b3c1d1d3d9074ad8026a5173a7792c1afb5
pihme <pihme@users.noreply.github.com>
2022-06-17 10:22:02 +0200

    humble beginnings

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use gateway_protocol::gateway_client::GatewayClient;
+use gateway_protocol::TopologyRequest;
+
+pub mod gateway_protocol {
+    tonic::include_proto!("gateway_protocol");
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut client = GatewayClient::connect("http://[::1]:26500").await?;
+
+    let response = client.topology(TopologyRequest {}).await;
+
+    match response {
+        Ok(response) => {
+            let topology = response.into_inner();
+            println!("SUMMARY: {:?}", topology);
+            println!("ClusterSize: {:?}", topology.cluster_size)
+        }
+        Err(e) => println!("something went wrong: {:?}", e),
+    }
+
+    Ok(())
 }
