952e80dd6a63253e657b487ab964b0f5a918c304
Ole Schönburg <ole.schoenburg@gmail.com>
2022-08-10 12:45:39 +0200

    feat: implement publish message command

--- cli/src/publish.rs
+++ cli/src/publish.rs
@@ -3,7 +3,7 @@ use std::fmt::Debug;
 
 use clap::{Args, Subcommand};
 use tonic::transport::Channel;
-use zeebe_client::api::gateway_client::GatewayClient;
+use zeebe_client::api::{gateway_client::GatewayClient, PublishMessageRequest};
 
 #[derive(Args, Clone, Debug)]
 pub(crate) struct PublishArgs {
@@ -17,11 +17,45 @@ enum PublishResourceType {
 }
 
 #[derive(Args, Clone, Debug)]
-struct PublishMessageArgs {}
+struct PublishMessageArgs {
+    name: String,
+    #[clap(long)]
+    correlation_key: String,
+    #[clap(long)]
+    message_id: String,
+    #[clap(long, required = false, default_value = "")]
+    variables: String,
+    #[clap(long, required = false, default_value_t = -1)]
+    ttl: i64, // todo: should be duration
+}
+
+impl From<&PublishMessageArgs> for PublishMessageRequest {
+    fn from(args: &PublishMessageArgs) -> Self {
+        PublishMessageRequest {
+            name: args.name.to_owned(),
+            correlation_key: args.correlation_key.to_owned(),
+            time_to_live: args.ttl,
+            message_id: args.message_id.to_owned(),
+            variables: args.variables.to_owned(),
+        }
+    }
+}
 
 pub(crate) async fn handle_publish_command(
     client: &mut GatewayClient<Channel>,
     args: &PublishArgs,
 ) -> Result<Box<dyn Debug>> {
-    todo!()
+    match &args.resource_type {
+        PublishResourceType::Message(args) => handle_publish_message_command(client, args).await,
+    }
+}
+
+async fn handle_publish_message_command(
+    client: &mut GatewayClient<Channel>,
+    args: &PublishMessageArgs,
+) -> Result<Box<dyn Debug>> {
+    let request: PublishMessageRequest = args.into();
+    Ok(Box::new(
+        client.publish_message(request).await?.into_inner(),
+    ))
 }
